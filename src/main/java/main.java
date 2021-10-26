c3bce0dd82e9075f0ff9df72feeee81bd6b6609b
Krahll <andrea.innacolo@gmail.com>
2021-10-26 15:31:11 +0200

    Gestione URL

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,33 @@
 import org.apache.commons.validator.UrlValidator;
 
+import exception.InvalidUrlException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
-//        HashMap<String, T> dictionary = new HashMap<String, T>();
+
+    public static <T> void main(String[] args) throws InvalidUrlException {
+        HashMap<String, T> dictionary = new HashMap<String, T>();
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Ciao, inserisci una URL valida:");
+        String url = sc.nextLine();
 
         // 1) Check URL
         UrlValidator urlValidator = new UrlValidator();
-        urlValidator.isValid("http://my favorite site!");
-
+        
+        try {
+			if(urlValidator.isValid(url)) {
+			return;	
+			}
+		} catch (Exception e) {
+			if(!urlValidator.isValid(url)) {
+			e.printStackTrace();
+			throw new InvalidUrlException("Inserisci un URL valida!");
+			}
+		}
+        
+        
         // 2) Avvio ricerca link con inserimento db alla fine
 
         // 3) Aggiornamento mappa link con inserimento db alla fine
@@ -18,5 +35,10 @@ public class main {
         // 4) Avvio Ricerca mappe con inserimento db alla fine
 
         // 5)bho
+   
+    
+    
+    
+    
     }
 }
