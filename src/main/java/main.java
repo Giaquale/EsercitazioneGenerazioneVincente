bada88b0ded93877bd1661b5b0c61556fd537755
Alessandro Giaquinto Protom <71634385+Giaquale@users.noreply.github.com>
2021-10-26 11:40:57 +0200

    maven update

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,22 @@
+import org.apache.commons.validator.UrlValidator;
+
+import java.util.HashMap;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Ciao");
+        HashMap<String, T> dictionary = new HashMap<String, T>();
+        System.out.println("Ciao, inserisci una URL valida:");
+
+        // 1) Check URL
+        UrlValidator urlValidator = new UrlValidator();
+        urlValidator.isValid("http://my favorite site!");
+
+        // 2) Avvio ricerca link con inserimento db alla fine
+
+        // 3) Aggiornamento mappa link con inserimento db alla fine
+
+        // 4) Avvio Ricerca mappe con inserimento db alla fine
+
+        // 5)bho
     }
 }
