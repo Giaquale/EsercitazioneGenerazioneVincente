a48e2229863481dae4348715af506149cdd7ea53
cri347x <92524801+cri347x@users.noreply.github.com>
2021-10-26 14:58:00 +0200

    Aggiornamento

--- src/main/java/RunTestJSoupScraper.java
+++ src/main/java/RunTestJSoupScraper.java
@@ -2,23 +2,32 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+import exception.InvalidParameterException;
 import utils.JSoupScraper;
-
+/**
+ * Classe per il test di JSoupScraper
+ * @author Cristian Zanna
+ *
+ */
 public class RunTestJSoupScraper {
 	public static void main(String[] args) {
 		try {
-			JSoupScraper scraper = new JSoupScraper("https://it.wikipedia.org/wiki/Pagina_principale");
+			String urlValidata = "https://it.wikipedia.org/wiki/Pagina_principale";
+			JSoupScraper scraper = new JSoupScraper(urlValidata);
 			LinkedList<String> links = scraper.getLinks();
 			HashMap<String, String> images = scraper.getImages();
+			
+			scraper.rescrap();
 			
 			links.forEach(System.out::println);
 			
 			images.forEach((key, value) -> System.out.println("Nome: " + key + " Url: " + value));
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+		} catch (InvalidParameterException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}		
 	}
 }
